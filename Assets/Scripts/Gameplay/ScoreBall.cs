939f8bdda01ac812b74ec520630b9235d55f625c
Aleksey Seif <seif_brn@me.com>
2022-06-06 18:14:09 +0700

    add check on null

--- Assets/Scripts/Gameplay/ScoreBall.cs
+++ Assets/Scripts/Gameplay/ScoreBall.cs
@@ -17,7 +17,7 @@ public class ScoreBall : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.GetComponent<Ball>() != 0)
+        if(collision.gameObject.GetComponent<Ball>() != null)
         {
             BallLauncher.Instance.m_TempAmount++;    // increase balls amount
             PlayParticle();
